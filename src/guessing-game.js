33fe10e98a6256bf8180ac5a85e6aace06c655a0
Cl3r1k <Cl3r1k2@gmail.com>
2019-10-06 11:10:43 +0700

    feat: solve task

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,27 @@
 class GuessingGame {
-    constructor() {}
+
+    constructor() {
+        this.range = [];
+        this.searchIndex;
+    }
 
     setRange(min, max) {
-
+        this.range[0] = min;
+        this.range[1] = max;
+        this.searchIndex = 0;
     }
 
     guess() {
-
+        this.searchIndex = Math.ceil((this.range[0] + this.range[1]) / 2);
+        return this.searchIndex;
     }
 
     lower() {
-
+        this.range[1] = this.searchIndex;
     }
 
     greater() {
-
+        this.range[0] = this.searchIndex;
     }
 }
 
